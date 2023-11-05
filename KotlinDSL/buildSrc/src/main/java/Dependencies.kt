371fd2ae6db908b429602093ce7aa72f5d80583d
PauloLajos <paulolajos@gmail.com>
2023-11-05 07:29:55 +0100

    Gradle Dependency Management using Kotlin DSL https://write.agrevolution.in/gradle-dependency-management-using-kotlin-dsl-ab5b6184453f

---
+++ KotlinDSL/buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,8 @@
+object Dependencies {
+
+    const val gson = "com.google.code.gson:gson:${Versions.gson}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val kotlin_serializer =
+        "org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Versions.kotlin_serializer}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofit_converter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"}
---
+++ KotlinDSL/buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+
+    val gradle = "4.2.1"
+    const val gson = "2.8.7"
+    const val kotlin = "1.5.10"
+    const val retrofit = "2.9.0"
+    const val kotlin_serializer = "0.9.1"
+}
